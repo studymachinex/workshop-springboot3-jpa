bb522732316c7e354d47c6bac96e52af7bf5e844
Carlos Gabriel <carlosufpa10@gmail.com>
2023-01-02 20:45:25 -0300

    add user

--- src/main/java/com/ibm/course/resources/UserResource.java
+++ src/main/java/com/ibm/course/resources/UserResource.java
@@ -2,12 +2,20 @@ package com.ibm.course.resources;
 
 import com.ibm.course.entities.User;
 import com.ibm.course.services.UserService;
+
+import jakarta.servlet.Servlet;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,5 +34,17 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id){
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    //Add user
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+        .path("/{id}")
+        .buildAndExpand(obj.getId())
+        .toUri();
+
+        return ResponseEntity.created(uri).body(obj);
     }
 }
