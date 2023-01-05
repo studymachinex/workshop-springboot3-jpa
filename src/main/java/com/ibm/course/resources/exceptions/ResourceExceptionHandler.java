f6743ecd98b530f1f0a1eb3cefd9a24b8ad2395b
Carlos Gabriel <carlosufpa10@gmail.com>
2023-01-05 11:52:17 -0300

    :fireworks:

--- src/main/java/com/ibm/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/ibm/course/resources/exceptions/ResourceExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.ibm.course.services.exceptions.DataBaseException;
 import com.ibm.course.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,4 +23,13 @@ public class ResourceExceptionHandler {
         StandardError stdError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(stdError);
     }
+
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<StandardError> DataBaseException(DataBaseException e, HttpServletRequest request){
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError stdError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(stdError);
+    }
+
 }
--- src/main/java/com/ibm/course/services/UserService.java
+++ src/main/java/com/ibm/course/services/UserService.java
@@ -2,9 +2,14 @@ package com.ibm.course.services;
 
 import com.ibm.course.entities.User;
 import com.ibm.course.repositories.UserRepository;
+import com.ibm.course.services.exceptions.DataBaseException;
 import com.ibm.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
@@ -28,13 +33,24 @@ public class UserService {
     }
 
     public void deleteUser(Long id){
-        repository.deleteById(id);
+        try{
+            repository.deleteById(id);
+        }catch(EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }catch(DataIntegrityViolationException e){
+            throw new DataBaseException(e.getMessage());
+        }
     }
     
     public User updateUser(Long id, User user){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, user);
-        return repository.save(entity);
+        try{
+            User entity = repository.getReferenceById(id);
+            updateData(entity, user);
+            return repository.save(entity);
+        } 
+        catch(EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User user) {
